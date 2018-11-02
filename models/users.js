dfbebaea273c3ef72c5cf4628e7f8ee666040b84
Rahul Rawat <rahulrawat593@gmail.com>
2018-11-02 15:25:24 -0700

    Add graphQL shema and Naming for postgres

--- models/users.js
+++ models/users.js
@@ -1,21 +1,35 @@
-import Team from './teams'
 export default (sequelize, DataTypes) => {
-    const User = sequelize.define('user', {
-        username: {
-            type: DataTypes.STRING,
-            unique: true,
+    const User = sequelize.define(
+        'user',
+        {
+            username: {
+                type: DataTypes.STRING,
+                unique: true,
+            },
+            email: {
+                type: DataTypes.STRING,
+                unique: true,
+            },
+            password: DataTypes.STRING,
         },
-        email: {
-            type: DataTypes.STRING,
-            unique: true,
-        },
-        password: DataTypes.STRING,
-    });
+        { underscored: true },
+    );
 
     User.associate = (models) => {
-        models.User.belongsToMany(models.Team, {
+        User.belongsToMany(models.Team, {
             through: 'member',
-            foreignKey: 'userId',
+            foreignKey: {
+                name: 'userId',
+                field: 'user_id',
+            },
+        });
+        // N:M
+        User.belongsToMany(models.Team, {
+            through: 'channel_member',
+            foreignKey: {
+                name: 'userId',
+                field: 'user_id',
+            },
         });
     };
 
